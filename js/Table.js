36e67e41b4e3da28f96e469aaff3d82da4055423
Jakob Mass <jaks6@ut.ee>
2015-11-17 19:40:02 +0200

    Renamed misleading var name

--- js/Table.js
+++ js/Table.js
@@ -2,7 +2,7 @@ var TABLE_COLORS = {
 	cloth : "#4d9900"
 }
 
-var TABLE_LEN_Y = 140;
+var TABLE_LEN_Z = 140;
 var TABLE_LEN_X = 270;
 
 var TABLE_WALL_HEIGHT = 6;
@@ -19,7 +19,7 @@ Table.prototype.createMesh = function() {
 	var mesh = new THREE.Mesh();
 
 
-	var geometry = new THREE.PlaneGeometry(TABLE_LEN_X, TABLE_LEN_Y);
+	var geometry = new THREE.PlaneGeometry(TABLE_LEN_X, TABLE_LEN_Z);
 	geometry.computeFaceNormals();
 	geometry.computeVertexNormals();
 	
@@ -34,16 +34,16 @@ Table.prototype.createMesh = function() {
 	var leftWall = new THREE.Mesh(new THREE.PlaneGeometry(TABLE_LEN_X, TABLE_WALL_HEIGHT), material);
 	var rightWall = new THREE.Mesh(new THREE.PlaneGeometry(TABLE_LEN_X, TABLE_WALL_HEIGHT), material);
 
-	var topWall = new THREE.Mesh(new THREE.PlaneGeometry(TABLE_LEN_Y, TABLE_WALL_HEIGHT), material);
-	var bottomWall = new THREE.Mesh(new THREE.PlaneGeometry(TABLE_LEN_Y, TABLE_WALL_HEIGHT), material);
+	var topWall = new THREE.Mesh(new THREE.PlaneGeometry(TABLE_LEN_Z, TABLE_WALL_HEIGHT), material);
+	var bottomWall = new THREE.Mesh(new THREE.PlaneGeometry(TABLE_LEN_Z, TABLE_WALL_HEIGHT), material);
 
 	floor.rotation.set(-halfPi,0, 0);
 
 	topWall.rotation.set(0,halfPi, 0);
 	bottomWall.rotation.set(0,-halfPi, 0);
 
-	leftWall.position.set(0 , TABLE_WALL_HEIGHT / 2.0 , -TABLE_LEN_Y / 2.0);
-	rightWall.position.set(0 , TABLE_WALL_HEIGHT / 2.0 , TABLE_LEN_Y / 2.0);
+	leftWall.position.set(0 , TABLE_WALL_HEIGHT / 2.0 , -TABLE_LEN_Z / 2.0);
+	rightWall.position.set(0 , TABLE_WALL_HEIGHT / 2.0 , TABLE_LEN_Z / 2.0);
 
 	topWall.position.set(-TABLE_LEN_X / 2.0, TABLE_WALL_HEIGHT / 2.0, 0);
 	bottomWall.position.set(TABLE_LEN_X / 2.0, TABLE_WALL_HEIGHT / 2.0,0);
