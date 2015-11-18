a4108d21256fd53741b9ddd641b1998b53c7c017
Jakob Mass <jaks6@ut.ee>
2015-11-18 03:10:01 +0200

    Added cannon.js-based physics. Needs polishing.

--- js/Table.js
+++ js/Table.js
@@ -9,7 +9,65 @@ var TABLE_WALL_HEIGHT = 6;
 
 var Table = function() {
 	this.mesh = this.createMesh();
+	this.rigidBody = this.createBody(); //floor
+
+	// wall with normal of -z
+	createRotatedTableSidePlane(
+		new CANNON.Vec3(0,0, TABLE_LEN_Z / 2), 
+		new CANNON.Quaternion(0,1,0), 
+		Math.PI ); 
+
+	// wall with normal of +z
+	createRotatedTableSidePlane(
+		new CANNON.Vec3(0,0, -TABLE_LEN_Z / 2), 
+		new CANNON.Quaternion(0,1,0),
+		0 ); 
+
+	// wall with normal of -x
+	createRotatedTableSidePlane(
+		new CANNON.Vec3(TABLE_LEN_X / 2,0, 0), 
+		new CANNON.Quaternion(0,1,0), 
+		-Math.PI / 2 ); 
+
+	// wall with normal of +x
+	createRotatedTableSidePlane(
+		new CANNON.Vec3(-TABLE_LEN_X / 2,0, 0), 
+		new CANNON.Quaternion(0,1,0), 
+		Math.PI / 2 ); 
+
 	scene.add(this.mesh);
+}
+
+Table.prototype.createBody = function(){
+	var groundMaterial = new CANNON.Material();
+	var groundBody = new CANNON.Body({
+    	mass: 0, // mass == 0 makes the body static
+    	material: groundMaterial
+	});
+	var groundShape = new CANNON.Plane();
+	groundBody.addShape(groundShape);
+	//default plane normal is +z, rotate it on x-axis -90 deg
+	groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
+	world.addBody(groundBody);
+	return groundBody;
+}
+
+/**
+	Creates a plane, with a rotated normal instead of the the default +w 
+	@vector - cannon.vec3 for axis to rotate from
+	@ degree - degrees to rotate in radians
+*/
+var createRotatedTableSidePlane = function(position, vector, degree){
+	var groundBody = new CANNON.Body({
+    	mass: 0, // mass == 0 makes the body static
+    	material: new CANNON.Material()
+	});
+	groundBody.addShape(new CANNON.Plane());
+	//default plane normal is +z, rotate it on x-axis -90 deg
+	groundBody.quaternion.setFromAxisAngle(vector,degree);
+	groundBody.position = position;
+	world.addBody(groundBody);
+	return groundBody;
 
 }
 
