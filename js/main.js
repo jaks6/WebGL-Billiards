b22f246d00550bae6b91fa0e9a5e10ea971cedf9
jaks6 <jaks@ut.ee>
2015-12-12 13:13:43 +0200

    Changed shadow camera FOV to remove shadow anomaly.
    Because the shadow camera of a single light only covered part of the table
    and not all of it, the shadow disappeared with a clipping when the shadow
    exited the camera frustrum. Perhaps it's possible to make the shadow fade
    out instead of increasing the FOV like I did here?

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var WIDTH   = 960,
 
 //globals
 var renderer, scene, camera, game, controls, keyboard, lightsConfig, world;
-var debug = false; //if true then collision wireframes are drawn
+var debug = true; //if true then collision wireframes are drawn
 // instantiate a TextureLoader
 var textureLoader = new THREE.TextureLoader();
 
@@ -184,9 +184,11 @@ function addLights() {
     light1.target.updateMatrixWorld();
 
     light1.castShadow = true;
-    light1.shadowCameraFov = 70;
+    light1.shadowCameraFov = 110;
     light1.shadowCameraNear = 100;
     light1.shadowCameraFar = 160;
+    light1.shadowMapWidth = 1024;
+    light1.shadowMapHeight = 1024;
 
 
 
@@ -196,15 +198,19 @@ function addLights() {
     light2.target.updateMatrixWorld();
 
     light2.castShadow = true;
-    light2.shadowCameraFov = 70;
+    light2.shadowCameraFov = 110;
     light2.shadowCameraNear = 100;
     light2.shadowCameraFar = 160;
+    light2.shadowMapWidth = 1024;
+    light2.shadowMapHeight = 1024;
 
-    //for debugging
-    // var shadowCam1  = new THREE.CameraHelper(light1.shadow.camera);
-    // scene.add(shadowCam1);
-    // var shadowCam2  = new THREE.CameraHelper(light2.shadow.camera);
-    // scene.add(shadowCam2);
+    if (debug){
+        var shadowCam1  = new THREE.CameraHelper(light1.shadow.camera);
+        scene.add(shadowCam1);
+        var shadowCam2  = new THREE.CameraHelper(light2.shadow.camera);
+        scene.add(shadowCam2); 
+    }
+    
 
     scene.add(light1);
     scene.add(light2);
